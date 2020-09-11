11e4a15b13710b9b1d229952566dc3991d5983db
SimonMably <simonmably@gmail.com>
2020-09-11 20:15:02 +0100

    added alien_invasion.py, settings.py, ship.py, and an images folder containing ship.bmp

---
+++ settings.py
@@ -0,0 +1,53 @@
+class Settings:
+    """A class to store all the settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialise the game's settings."""
+        self.screen_width = 1500
+        self.screen_height = 750
+        self.bg_colour = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 1.5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
