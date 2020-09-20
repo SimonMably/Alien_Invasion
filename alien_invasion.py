750885a3d6a1368824a9937a2aa3ca609c9359e5
SimonMably <simonmably@gmail.com>
2020-09-20 15:04:34 +0100

    Changed settings in settings.py, added collision functionality to alien_invasion.py.

--- alien_invasion.py
+++ alien_invasion.py
@@ -86,6 +86,15 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        # Check for any bullets that have hit aliens.
+        # If so, get rid of the bullet and the alien.
+        collision = pygame.sprite.groupcollide(
+                                    self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            # Destroy existing bullets and create a new fleet.
+            self.bullets.empty()
+            self._create_fleet()
 
     def _update_aliens(self):
         """
